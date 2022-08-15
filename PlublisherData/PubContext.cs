4a5fc8954b7be1907bbbcb7a61f7c1266fe7b153
Josvany_G <josvanyg@gmail.com>
2022-08-15 11:24:34 -0600

    Agregar archivos de proyecto.

---
+++ PlublisherData/PubContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlublisherData
+{
+    public class PubContext : DbContext
+    {
+        public PubContext() : base()
+        {
+
+        }
+
+        public DbSet<Author> Authors { get; set; }
+    }
+}
